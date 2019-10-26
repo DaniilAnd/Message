d0d6b97e82ea1ada5d302d396870adaa6cc9d2d1
DaniilAnd <lisichkindan@yandex.ru>
2019-10-26 09:57:18 +0300

    Add project

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(Data data)
+        {
+            if(ModelState.IsValid){
+                return View("Thanks", data);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
